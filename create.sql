7f2a31959c44b30d904b73ae5b8783ac4f4a7ff5
Arseniy <arsenshapar@gmail.com>
2023-11-18 01:02:08 +0200

    changed create.sql, populate.sql, query.sql, relational_schema.png

--- create.sql
+++ create.sql
@@ -1,28 +1,32 @@
 CREATE TABLE players
 (
-	player_id SERIAL PRIMARY KEY,
-	username VARCHAR(100) NOT NULL,
-	rating INT NOT NULL
+	player_id VARCHAR(150) NOT NULL,
+	rating INT NOT NULL,
+	reg_date DATE NOT NULL,
+	sex CHAR(1),
+	PRIMARY KEY (player_id)
 );
 
 CREATE TABLE openings
 (
-	eco VARCHAR(10) PRIMARY KEY,
-	opening_name VARCHAR(50) NOT NULL,
-	opening_ply INT NOT NULL
+	eco VARCHAR(10) NOT NULL,
+	opening_name VARCHAR(150) NOT NULL,
+	opening_ply INT NOT NULL,
+	PRIMARY KEY (eco)
 );
 
 CREATE TABLE games
 (
-	game_id CHAR(8) PRIMARY KEY,
-	game_date TIMESTAMP NOT NULL,
-	time_control VARCHAR(10) NOT NULL,
+	game_id CHAR(8) NOT NULL,
+	game_date DATE NOT NULL,
+	time_control VARCHAR(5) NOT NULL,
 	num_turns INT NOT NULL,
-	game_result VARCHAR(10) NOT NULL,
-	winner VARCHAR(5) NOT NULL,
-	white_player INT NOT NULL,
-	black_player INT NOT NULL,
+	victory_status VARCHAR(10) NOT NULL,
+	winner CHAR(5) NOT NULL,
+	white_player VARCHAR(150) NOT NULL,
+	black_player VARCHAR(150) NOT NULL,
 	eco VARCHAR(10) NOT NULL,
+	PRIMARY KEY (game_id),
 	FOREIGN KEY (white_player) REFERENCES players(player_id),
 	FOREIGN KEY (black_player) REFERENCES players(player_id),
 	FOREIGN KEY (eco) REFERENCES openings(eco)
--- query.sql
+++ query.sql
@@ -1,15 +1,15 @@
--- Вивести id партіЇ, гравців та переможця з усіх партій гравця 'daniel_likes_chess', в яких він переміг
--- SELECT * FROM 
--- (SELECT DISTINCT game_id, (SELECT username FROM players WHERE player_id = white_player) AS w_player, (SELECT username FROM players WHERE player_id = black_player) AS b_player, winner 
--- FROM games JOIN players ON player_id IN (white_player, black_player)) AS r 
--- WHERE (w_player = 'daniel_likes_chess' AND winner = 'white') OR (b_player = 'daniel_likes_chess' AND winner = 'black');
+-- Кількість партій, в яких був розіграний кожен дебют
+SELECT eco, opening_name, COUNT(game_id) AS games_count FROM games RIGHT JOIN openings USING(eco) GROUP BY eco;
 
--- Вивести контроль часу, переможця(колір) та кількість ходів  з усіх партій, в яких був розігран дебют 'Nimzowitsch Defense: Kennedy Variation'
--- SELECT time_control, winner, num_turns, opening_name FROM games JOIN openings USING(eco) 
--- WHERE opening_name = 'Nimzowitsch Defense: Kennedy Variation';
 
--- Висести id партіЇ, гравців, переможця та назву дебюту з усіх партій гравця 'a-00', в яких він грав чорними та було розіграно дебют 'Slav Defense: Exchange Variation' або 'King's Pawn Game: Leonardis Variation'
--- SELECT * FROM 
--- (SELECT DISTINCT game_id, (SELECT username FROM players WHERE player_id = white_player) AS w_player, (SELECT username FROM players WHERE player_id = black_player) AS b_player, winner, opening_name 
--- FROM games JOIN players ON (player_id IN (white_player, black_player)) JOIN openings USING(eco)) AS r 
--- WHERE b_player = 'a-00' AND opening_name IN ('Slav Defense: Exchange Variation', 'King''s Pawn Game: Leonardis Variation');
+-- Кількість перемог кожної сторони(або нічиїх)
+SELECT (SELECT COUNT(*) FROM games WHERE winner = 'white') AS white_wins, 
+	   (SELECT COUNT(*) FROM games WHERE winner = 'black') AS black_wins,
+	   (SELECT COUNT(*) FROM games WHERE winner = 'draw') AS draws;
+
+
+-- Сумарна кількість перемог гравців з кожного рейтинга
+SELECT rating, SUM(wins) AS wins FROM 
+(SELECT rating, (SELECT COUNT(*) FROM games WHERE (white_player = player_id AND winner = 'white') OR (black_player = player_id AND winner = 'black')) AS wins FROM players) AS r
+GROUP BY rating
+ORDER BY rating;
